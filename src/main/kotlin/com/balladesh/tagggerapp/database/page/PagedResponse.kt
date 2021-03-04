0cc2915ea80977e2fcd623b8eb14daccfb7c8410
BrenoLopes <brenolprimo@gmail.com>
2021-03-04 22:14:26 +0000

    Updated: Inserted method to check for equality and toString

--- src/main/kotlin/com/balladesh/tagggerapp/database/page/PagedResponse.kt
+++ src/main/kotlin/com/balladesh/tagggerapp/database/page/PagedResponse.kt
@@ -13,4 +13,30 @@ class PagedResponse<T>(
 
       return if (total == 0) { 1 } else { total }
     }
+
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (javaClass != other?.javaClass) return false
+
+    other as PagedResponse<*>
+
+    if (page != other.page) return false
+    if (pageSize != other.pageSize) return false
+    if (itemsList != other.itemsList) return false
+    if (totalPages != other.totalPages) return false
+
+    return true
+  }
+
+  override fun hashCode(): Int {
+    var result = page
+    result = 31 * result + pageSize
+    result = 31 * result + itemsList.hashCode()
+    result = 31 * result + totalPages
+    return result
+  }
+
+  override fun toString(): String {
+    return "PagedResponse(page=$page, pageSize=$pageSize, itemsList=$itemsList, totalPages=$totalPages)"
+  }
 }
