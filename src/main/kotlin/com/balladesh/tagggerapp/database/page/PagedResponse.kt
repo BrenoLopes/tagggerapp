6ff2a9ec1a38fc6d4180625b9646a789e8d0cdd1
BrenoLopes <brenolprimo@gmail.com>
2021-03-04 22:04:37 +0000

    Updated: Changed totalItems from Int to Long

--- src/main/kotlin/com/balladesh/tagggerapp/database/page/PagedResponse.kt
+++ src/main/kotlin/com/balladesh/tagggerapp/database/page/PagedResponse.kt
@@ -3,15 +3,14 @@ package com.balladesh.tagggerapp.database.page
 class PagedResponse<T>(
   val page: Int,
   val pageSize: Int,
-  private val totalItems: Int,
+  private val totalItems: Long,
   val itemsList: List<T>
 ) {
 
   val totalPages: Int
     get() {
-      val total: Int = if (pageSize == 0) { 1 } else { totalItems / pageSize }
+      val total: Int = if (pageSize == 0) { 1 } else { (totalItems / pageSize).toInt() }
 
       return if (total == 0) { 1 } else { total }
     }
-
 }
