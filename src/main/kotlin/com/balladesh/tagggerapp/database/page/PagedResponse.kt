fad286a5b2fcfc00c02ccae469368c881f3a43e4
BrenoLopes <brenolprimo@gmail.com>
2021-03-08 22:16:22 +0000

    Added javadoc for the PagedResponse

--- src/main/kotlin/com/balladesh/tagggerapp/database/page/PagedResponse.kt
+++ src/main/kotlin/com/balladesh/tagggerapp/database/page/PagedResponse.kt
@@ -3,15 +3,27 @@ package com.balladesh.tagggerapp.database.page
 import com.balladesh.tagggerapp.database.session.DatabaseSession
 import javax.persistence.Query
 
+/**
+ * A class to help the user navigate between the entire query, when the database have too many items
+ * and the user needs to break it in multiple small places instead of getting the entire data in one go.
+ *
+ * @param page current page that this data is fragmented into
+ * @param pageSize maximum amount of data in one page. Use 0 or -1 to get the entire data in one query
+ * @param totalItems the total count of data that the database has
+ * @param dataList the data inside a list
+ */
 class PagedResponse<T>(
   val page: Int,
   val pageSize: Int,
   private val totalItems: Long,
-  val itemsList: List<T>
+  val dataList: List<T>
 ) {
+  /**
+   * Calculates the total number of pages that the data is fragmented into
+   */
   val totalPages: Int
     get() {
-      val total: Int = if (pageSize == 0) {
+      val total: Int = if (pageSize < 1) {
         1
       } else {
         ((totalItems - 1) / pageSize).toInt() + 1
@@ -28,7 +40,7 @@ class PagedResponse<T>(
 
     if (page != other.page) return false
     if (pageSize != other.pageSize) return false
-    if (itemsList != other.itemsList) return false
+    if (dataList != other.dataList) return false
     if (totalPages != other.totalPages) return false
 
     return true
@@ -37,25 +49,31 @@ class PagedResponse<T>(
   override fun hashCode(): Int {
     var result = page
     result = 31 * result + pageSize
-    result = 31 * result + itemsList.hashCode()
+    result = 31 * result + dataList.hashCode()
     result = 31 * result + totalPages
     return result
   }
 
   override fun toString(): String {
-    return "PagedResponse(page=$page, pageSize=$pageSize, itemsList=$itemsList, totalPages=$totalPages)"
+    return "PagedResponse(page=$page, pageSize=$pageSize, itemsList=$dataList, totalPages=$totalPages)"
   }
 }
 
+/**
+ * A factory to quickly query and create a response if the user needs it to be fragmented
+ */
 interface PagedResponseFactory<T> {
   fun create(dbSession: DatabaseSession, fetchQuery: Query, countQuery: Query, pageable: Pageable): PagedResponse<T>
 }
 
+/**
+ * A default implementation for the factory. It limits the query if the maximum page size is more then 0
+ */
 class DefaultResponseFactory<T>: PagedResponseFactory<T> {
   override fun create(dbSession: DatabaseSession, fetchQuery: Query, countQuery: Query, pageable: Pageable): PagedResponse<T> {
     if (pageable.maxSize > 0) {
       fetchQuery.maxResults = pageable.maxSize
-      fetchQuery.firstResult = if (pageable.page > 0) pageable.page * pageable.maxSize else 0
+      fetchQuery.firstResult = if (pageable.page > 0) { pageable.page * pageable.maxSize } else { 0 }
     }
 
     @Suppress("UNCHECKED_CAST")
