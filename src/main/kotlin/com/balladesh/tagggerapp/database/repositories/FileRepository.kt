57487a855bcf9908165fdcfedb209a341e219732
BrenoLopes <brenolprimo@gmail.com>
2021-03-08 17:09:32 +0000

    Added: A repository for the File entity

---
+++ src/main/kotlin/com/balladesh/tagggerapp/database/repositories/FileRepository.kt
@@ -0,0 +1,199 @@
+package com.balladesh.tagggerapp.database.repositories
+
+import com.balladesh.tagggerapp.database.DatabaseConnection
+import com.balladesh.tagggerapp.database.entities.File
+import com.balladesh.tagggerapp.database.entities.Tag
+import com.balladesh.tagggerapp.database.page.*
+import com.balladesh.tagggerapp.database.session.DatabaseSessionBuilder
+import java.util.Optional
+import javax.persistence.EntityExistsException
+import javax.persistence.EntityTransaction
+import javax.persistence.NoResultException
+import javax.transaction.Transaction
+
+class FileRepository(private val responseFactory: PagedResponseFactory<File> = DefaultResponseFactory()) {
+  private var databaseSessionBuilder: DatabaseSessionBuilder
+
+  init {
+    this.databaseSessionBuilder = DatabaseSessionBuilder(DatabaseConnection().persistenceUnitName)
+  }
+
+  constructor(sessionBuilder: DatabaseSessionBuilder) : this() {
+    this.databaseSessionBuilder = sessionBuilder
+  }
+
+  /**
+   * Select all files present inside the database
+   *
+   * @param pageable optional param to determine the amount to be fetched from the database
+   *
+   * @return List of files in the database
+   */
+  fun findAll(pageable: Pageable = Pageable()): PagedResponse<File> {
+    val session = this.databaseSessionBuilder.build()
+
+    try {
+      val fetchQuery = session.manager.createQuery("From File f ORDER BY f.name ${pageable.sort}")
+      val countQuery = session.manager.createQuery("SELECT COUNT(f.name) FROM File f")
+
+      return this.responseFactory.create(session, fetchQuery, countQuery, pageable)
+    } catch (e: NoResultException) {
+      return PagedResponse(pageable.page, pageable.maxSize, 0, emptyList())
+    } catch (e: Exception) {
+      throw RuntimeException("Could not query the database: ${e.message}")
+    } finally {
+      session.close()
+    }
+  }
+
+  /**
+   * Find a file by it's given id.
+   *
+   * @param id the id to be searched
+   *
+   * @throws IllegalStateException if the entity manager couldn't be initialized
+   * @throws RuntimeException if an error happened during the search
+   */
+  fun findById(id: Long): Optional<File> {
+    val session = this.databaseSessionBuilder.build()
+
+    try {
+      val file = session.manager.find(File::class.java, id)
+
+      return Optional.ofNullable(file)
+    } catch(e: Exception) {
+      throw RuntimeException("Could not query the database: ${e.message}")
+    } finally {
+      session.close()
+    }
+  }
+
+  /**
+   * Find a file by it's name
+   *
+   * @param name the file's name to be searched
+   *
+   * @throws IllegalStateException if the entity manager couldn't be initialized
+   * @throws RuntimeException if an error happened during the search
+   */
+  fun findByName(name: String, pageable: Pageable = Pageable()): PagedResponse<File> {
+    val session = this.databaseSessionBuilder.build()
+
+    try {
+      val countQuery = session.manager.createQuery("SELECT COUNT(f.name) FROM File f")
+      val fetchQuery = session.manager.createQuery("FROM File f WHERE f.name = :name ORDER BY f.name ${pageable.sort}")
+      fetchQuery.setParameter("name", name)
+
+      return this.responseFactory.create(session, fetchQuery, countQuery, pageable)
+    } catch (e: Exception) {
+      throw RuntimeException("Could not query the database: ${e.message}")
+    } finally {
+      session.close()
+    }
+  }
+
+  /**
+   * Find a file by it's association with a tag
+   *
+   * @param tag the tag to be used in the search
+   *
+   * @throws IllegalStateException if the entity manager couldn't be initialized
+   * @throws RuntimeException if an error happened during the search
+   */
+  fun findByTag(tag: Tag, pageable: Pageable = Pageable()): PagedResponse<File> {
+    val session = this.databaseSessionBuilder.build()
+
+    try {
+      val countQuery = session.manager.createQuery("SELECT COUNT(f.name) FROM File f")
+      val fetchQuery = session.manager.createQuery("SELECT f FROM File f, IN(f.tags) t WHERE t = :tag ORDER BY f.name ${pageable.sort}")
+      fetchQuery.setParameter("tag", tag)
+
+      return this.responseFactory.create(session, fetchQuery, countQuery, pageable)
+    } catch (e: Exception) {
+      throw RuntimeException("Could not query the database: ${e.message}")
+    } finally {
+      session.close()
+    }
+  }
+
+  /**
+   * Persist a file in the database
+   *
+   * @param files the files to be persisted
+   *
+   * @throws EntityExistsException if the entity already exists in the database
+   * @throws RuntimeException if the persistance fails at runtime
+   */
+  fun persistFile(vararg files: File) {
+    val databaseSession = this.databaseSessionBuilder.build()
+    val transaction = databaseSession.manager.transaction
+
+    try {
+      transaction.begin()
+      files.forEach { databaseSession.manager.persist(it) }
+      transaction.commit()
+    } catch (e: EntityExistsException) {
+      this.rollbackTransactionIfActive(transaction)
+      throw e
+    } catch(e: Exception) {
+      this.rollbackTransactionIfActive(transaction)
+      throw RuntimeException("Could not persist the file on the database: ${e.message}")
+    } finally {
+      databaseSession.close()
+    }
+  }
+
+  /**
+   * Updates a file in the database
+   *
+   * @param files the files to be updated
+   *
+   * @return RuntimeException if an error happened at runtime
+   */
+  fun updateFile(vararg files: File) {
+    val databaseSession = this.databaseSessionBuilder.build()
+    val transaction = databaseSession.manager.transaction
+
+    try {
+      transaction.begin()
+      files.forEach { databaseSession.manager.merge(it) }
+      transaction.commit()
+    } catch (e: Exception) {
+      this.rollbackTransactionIfActive(transaction)
+      throw RuntimeException("Could not update the file on the database: ${e.message}")
+    } finally {
+        databaseSession.close()
+    }
+  }
+
+  /**
+   * Remove a file from the database
+   *
+   * @param files the files to be removed
+   *
+   * @throws RuntimeException if an error happens at runtime
+   */
+  fun removeFile(vararg files: File) {
+    val databaseSession = this.databaseSessionBuilder.build()
+    val transaction = databaseSession.manager.transaction
+
+    try {
+      transaction.begin()
+      files.forEach {
+        databaseSession.manager.remove(databaseSession.manager.find(File::class.java, it.id))
+      }
+      transaction.commit()
+    } catch(e: Exception) {
+      this.rollbackTransactionIfActive(transaction)
+      throw RuntimeException("Could not remove the file from the database: ${e.message}")
+    } finally {
+      databaseSession.close()
+    }
+  }
+
+  private fun rollbackTransactionIfActive(transaction: EntityTransaction) {
+    if (!transaction.isActive) return
+
+    transaction.rollback()
+  }
+}
---
+++ src/test/kotlin/com/balladesh/tagggerapp/database/repositories/FileRepositoryTest.kt
@@ -0,0 +1,233 @@
+package com.balladesh.tagggerapp.database.repositories
+
+import com.balladesh.tagggerapp.database.TestDatabaseConnection
+import com.balladesh.tagggerapp.database.entities.File
+import com.balladesh.tagggerapp.database.entities.Tag
+import com.balladesh.tagggerapp.database.page.Pageable
+import com.balladesh.tagggerapp.database.page.PagedResponse
+import com.balladesh.tagggerapp.database.page.Sort
+import com.balladesh.tagggerapp.database.session.DatabaseSessionBuilder
+import org.junit.ComparisonFailure
+import java.time.LocalDateTime
+import javax.persistence.NoResultException
+import kotlin.test.AfterTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+internal class FileRepositoryTest {
+  private val testTarget: FileRepository
+  private val dbBuilder: DatabaseSessionBuilder
+
+  init {
+    val sessionBuilder = DatabaseSessionBuilder(TestDatabaseConnection().persistenceUnitName)
+
+    this.dbBuilder = DatabaseSessionBuilder(TestDatabaseConnection().persistenceUnitName)
+    this.testTarget = FileRepository(sessionBuilder)
+  }
+
+  @AfterTest
+  fun destroyData() {
+    this.cleanDatabase()
+  }
+
+  @Test
+  fun testFindAllWithoutPaging() {
+    val file01 = File("file01", "pathfile01", LocalDateTime.now(), arrayListOf())
+    val file02 = File("file02", "pathfile02", LocalDateTime.now(), arrayListOf())
+    val file03 = File("file03", "pathfile03", LocalDateTime.now(), arrayListOf())
+    val file04 = File("file04", "pathfile04", LocalDateTime.now(), arrayListOf())
+    val file05 = File("file05", "pathfile05", LocalDateTime.now(), arrayListOf())
+    val file06 = File("file06", "pathfile06", LocalDateTime.now(), arrayListOf())
+
+    this.testTarget.persistFile(file01, file02, file03, file04, file05, file06)
+
+    val expected = PagedResponse(0, 0, 6, listOf(file01, file02, file03, file04, file05, file06))
+    val received = this.testTarget.findAll()
+
+    assertEquals(expected, received)
+  }
+
+  @Test
+  fun testFindAllByAccessingTheFirstPage() {
+    val file01 = File("file01", "pathfile01", LocalDateTime.now(), arrayListOf())
+    val file02 = File("file02", "pathfile02", LocalDateTime.now(), arrayListOf())
+    val file03 = File("file03", "pathfile03", LocalDateTime.now(), arrayListOf())
+    val file04 = File("file04", "pathfile04", LocalDateTime.now(), arrayListOf())
+    val file05 = File("file05", "pathfile05", LocalDateTime.now(), arrayListOf())
+    val file06 = File("file06", "pathfile06", LocalDateTime.now(), arrayListOf())
+
+    this.testTarget.persistFile(file01, file02, file03, file04, file05, file06)
+
+    val expectedResponse = PagedResponse(
+      0,
+      5,
+      6,
+      listOf(file01, file02, file03, file04, file05)
+    )
+    val receivedResponse = this.testTarget.findAll(Pageable(0, 5, Sort.ASC))
+
+    val expectedTotalPages = 2
+    val receivedTotalPages = receivedResponse.totalPages
+
+    assertEquals(expectedResponse, receivedResponse)
+    assertEquals(expectedTotalPages, receivedTotalPages)
+  }
+
+  @Test
+  fun testFindAllByAccessingTheSecondPage() {
+    val file01 = File("file01", "pathfile01", LocalDateTime.now(), arrayListOf())
+    val file02 = File("file02", "pathfile02", LocalDateTime.now(), arrayListOf())
+    val file03 = File("file03", "pathfile03", LocalDateTime.now(), arrayListOf())
+    val file04 = File("file04", "pathfile04", LocalDateTime.now(), arrayListOf())
+    val file05 = File("file05", "pathfile05", LocalDateTime.now(), arrayListOf())
+    val file06 = File("file06", "pathfile06", LocalDateTime.now(), arrayListOf())
+
+    this.testTarget.persistFile(file01, file02, file03, file04, file05, file06)
+
+    val expectedResponse = PagedResponse(
+      1,
+      5,
+      6,
+      listOf(file06)
+    )
+    val receivedResponse = this.testTarget.findAll(Pageable(1, 5, Sort.ASC))
+
+    val expectedTotalPages = 2
+    val receivedTotalPages = receivedResponse.totalPages
+
+    assertEquals(expectedResponse, receivedResponse)
+    assertEquals(expectedTotalPages, receivedTotalPages)
+  }
+
+  @Test
+  fun testFindAllWithPagingInDescending() {
+    val file01 = File("file01", "pathfile01", LocalDateTime.now(), arrayListOf())
+    val file02 = File("file02", "pathfile02", LocalDateTime.now(), arrayListOf())
+    val file03 = File("file03", "pathfile03", LocalDateTime.now(), arrayListOf())
+    val file04 = File("file04", "pathfile04", LocalDateTime.now(), arrayListOf())
+    val file05 = File("file05", "pathfile05", LocalDateTime.now(), arrayListOf())
+    val file06 = File("file06", "pathfile06", LocalDateTime.now(), arrayListOf())
+
+    this.testTarget.persistFile(file01, file02, file03, file04, file05, file06)
+
+    val expectedResponse = PagedResponse(
+      0,
+      5,
+      6,
+      listOf(file06, file05, file04, file03, file02)
+    )
+    val receivedResponse = this.testTarget.findAll(Pageable(0, 5, Sort.DESC))
+
+    val expectedTotalPages = 2
+    val receivedTotalPages = receivedResponse.totalPages
+
+    assertEquals(expectedResponse, receivedResponse)
+    assertEquals(expectedTotalPages, receivedTotalPages)
+  }
+
+  @Test
+  fun testSearchById() {
+    val expectedFile = File("file01", "pathfile01", LocalDateTime.now(), arrayListOf())
+    this.testTarget.persistFile(expectedFile)
+
+    val receivedFile = this.testTarget.findById(expectedFile.id)
+      .orElseThrow { NoResultException::class.java.newInstance() }
+
+    assertEquals(expectedFile, receivedFile)
+  }
+
+  @Test
+  fun testFindByName() {
+    val file01 = File("file01", "pathfile01", LocalDateTime.now(), arrayListOf())
+    val file02 = File("file01", "pathfile02", LocalDateTime.now(), arrayListOf())
+
+    this.testTarget.persistFile(file01, file02)
+
+    val expected = PagedResponse(
+      0,
+      0,
+      2,
+      listOf(file01, file02)
+    )
+    val received = this.testTarget.findByName("file01")
+
+    assertEquals(expected, received)
+  }
+
+  @Test
+  fun testFindByNameWithPaging() {
+    val file01 = File("file01", "pathfile01", LocalDateTime.now(), arrayListOf())
+    val file02 = File("file01", "pathfile02", LocalDateTime.now(), arrayListOf())
+
+    this.testTarget.persistFile(file01, file02)
+
+    val expected = PagedResponse(
+      0,
+      1,
+      2,
+      listOf(file01)
+    )
+    val received = this.testTarget.findByName("file01", Pageable(0, 1, Sort.ASC))
+
+    assertEquals(expected, received)
+  }
+
+  @Test
+  fun testFindByTagWithoutPaging() {
+    val tag = Tag("JustATag")
+
+    val file01 = File("file01", "path01")
+    val file02 = File("file02", "path02")
+
+    tag.addFiles(file01, file02)
+    testTarget.persistFile(file01, file02)
+
+    val expected = PagedResponse(0, 0, 2, listOf(file01, file02))
+
+    val received = testTarget.findByTag(tag)
+
+    assertEquals(expected, received)
+  }
+
+  @Test
+  fun testUpdate() {
+    val file01 = File("file01", "path01")
+    testTarget.persistFile(file01)
+
+    file01.name = "editedfile01"
+    testTarget.updateFile(file01)
+
+    val received = testTarget.findById(file01.id).orElseThrow { ComparisonFailure::class.java.newInstance() }
+
+    assertEquals(file01, received)
+  }
+
+  @Test
+  fun testDelete() {
+    val file01 = File("file01", "path01")
+    val file02 = File("file02", "path02")
+
+    testTarget.persistFile(file01, file02)
+    testTarget.removeFile(file01, file02)
+
+    val expected = PagedResponse<File>(0, 0, 0, emptyList())
+    val received = testTarget.findAll()
+
+    assertEquals(expected, received)
+  }
+
+  private fun cleanDatabase() {
+    val session = this.dbBuilder.build()
+
+    val transaction = session.manager.transaction
+
+    transaction.begin()
+    val delTagQuery = session.manager.createQuery("DELETE FROM Tag")
+    val delFileQuery = session.manager.createQuery("DELETE FROM File")
+
+    delTagQuery.executeUpdate()
+    delFileQuery.executeUpdate()
+    session.flush()
+    transaction.commit()
+  }
+}
